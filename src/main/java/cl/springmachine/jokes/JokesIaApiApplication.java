59c1ce9ea3203ddd49f59d3399ed595ae60ea1cc
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-07-06 20:30:36 -0400

    Migrate project to java

---
+++ src/main/java/cl/springmachine/jokes/JokesIaApiApplication.java
@@ -0,0 +1,13 @@
+package cl.springmachine.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesIaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesIaApiApplication.class, args);
+	}
+
+}
